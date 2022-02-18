7072e92f3aa6f85aabdf4b5daf9ad4685c5b8eaa
Vivaan Baid <baidvivaan782@gmail.com>
2022-02-17 19:06:28 -0500

    first commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Vivaan Baid on 17/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    //Total Per Person calculation done here
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+
+    }
+    
+    var curr:  FloatingPointFormatStyle<Double>.Currency{
+        return .currency(code: Locale.current.currencyCode ?? "USD")
+    }
+    
+    var totalAmount: Double{
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + (Double(numberOfPeople)*tipValue)
+    }
+    
+    var body: some View {
+        NavigationView{
+        Form{
+            Section{
+                TextField("Amount", value: $checkAmount, format: curr)
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                Picker("Number of people", selection: $numberOfPeople){
+                    ForEach(2..<100){
+                        Text("\($0) people")
+                    }
+                }
+                .pickerStyle(.automatic)
+            }
+            Section{
+                Picker("Tip", selection: $tipPercentage){
+                    ForEach(0..<101, id: \.self){
+                        Text($0, format: .percent)
+                    }
+                }
+                .pickerStyle(.automatic)
+                
+            }header: {
+                Text("Do not be a bitch and tip")
+            }
+            Section {
+                Text(totalPerPerson, format: curr)
+            }header: {
+                Text("Total Tip:")
+            }
+            Section{
+                Text(totalAmount, format: curr)
+            }
+            
+          }
+           .navigationTitle("Wesplit")
+           .toolbar {
+               ToolbarItemGroup(placement: .keyboard) {
+                   Button("Done") {
+                       amountIsFocused = false
+                   }
+               }
+           }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
